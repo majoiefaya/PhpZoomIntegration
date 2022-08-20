4ee674cde0c3761623cb3a7967608fbe02901330
Majoie Faya <majoiefaya@gmail.com>
2022-08-20 01:53:58 +0000

    First Commit

---
+++ Zoom/config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+  
+define('CLIENT_ID', 'gP0U57wMRzmy9Z3n6ij5XA');
+define('CLIENT_SECRET', 'CJr3TdgMoJ5OUvvt8gpPvhLXoxa8g9Q9');
+define('REDIRECT_URI', 'https://9e5b-196-171-2-157.eu.ngrok.io/zoom/callback.php');
