d6c39a22c4d778aa8b8f9d8a5c4be96881cbf0db
MajoieFaya <majoiefaya@gmail.com>
2022-08-20 02:11:41 +0000

    Update config.php

--- Zoom/config.php
+++ Zoom/config.php
@@ -2,6 +2,6 @@
 require_once 'vendor/autoload.php';
 require_once "class-db.php";
   
-define('CLIENT_ID', 'gP0U57wMRzmy9Z3n6ij5XA');
-define('CLIENT_SECRET', 'CJr3TdgMoJ5OUvvt8gpPvhLXoxa8g9Q9');
+define('CLIENT_ID', '');
+define('CLIENT_SECRET', '');
 define('REDIRECT_URI', 'https://9e5b-196-171-2-157.eu.ngrok.io/zoom/callback.php');
